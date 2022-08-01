521624dbaabc107d62905f3991c6b95675d7d532
Cornelius Renken <cornelius.renken@gmail.com>
2022-08-01 10:39:35 -0700

    change server code to forward requests

--- server/index.js
+++ server/index.js
@@ -1,19 +1,37 @@
 /* eslint-disable no-loop-func */
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 require('dotenv').config();
+const morgan = require('morgan');
+
 
 const { getAll } = require('./getAll.js');
 
 const app = express();
+app.use(morgan('tiny'));
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-app.get('/test', (req, res) => {
-  res.send('it works');
-});
 app.get('/qa/questions/:id/all/:page', getAll);
+
+app.all('/*', (req, res) => {
+  console.log(req);
+  axios({
+    url: req.url,
+    baseURL: 'http://localhost:6246/',
+    method: req.method,
+    data: req.body,
+  })
+    .then((response) => {
+      res.status(response.status).send(response.data);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 
 app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`));
